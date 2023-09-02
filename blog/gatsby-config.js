52d909889d8dcf5eeedb6dae3e75afd166430392
Chris Chang Change <changc21@gmatc.matc.edu>
2023-09-02 17:40:50 -0500

    Finished activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Chris Chang's Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog.`,
+    author: `Chris Chang`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact:{
+      name: `Chris Chang`, 
+      company: `Chang Gang`,
+      address: `0921 Chang Blv`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
